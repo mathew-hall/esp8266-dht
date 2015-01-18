24c24a0f93323de52eaf0887196983283357b893
Mathew Hall <mathew.james.hall+gh@gmail.com>
2015-01-18 01:57:32 +0000

    Fall back on a hard-coded IP when DNS doesn't work

--- user/tempd.c
+++ user/tempd.c
@@ -4,6 +4,7 @@
 #include "espconn.h"
 #include "mem.h"
 #include "osapi.h"
+#include "ip_addr.h"
 
 #include "io.h"
 #include "tempd.h"
@@ -28,6 +29,7 @@ static ip_addr_t master_addr;
 
 #define DATA_PORT 19252
 #define DATA_HOST "bmo.local"
+#define FALLBACK_IP 10,0,0,123
 
 static ETSTimer broadcastTimer;
 
@@ -39,7 +41,11 @@ static void transmitReading(struct sensor_reading* result){
 static void broadcastReading(void* arg){
 
     os_printf("Sending heartbeat\n");
-    struct sensor_reading* result = readDHT();
+#ifdef SLEEP_MODE
+    struct sensor_reading* result = readDHT(1);
+#else
+    struct sensor_reading* result = readDHT(0);
+#endif
     transmitReading(result);    
     result = readDS18B20();
     transmitReading(result);
@@ -48,16 +54,24 @@ static void broadcastReading(void* arg){
 
 static void dnsLookupCb(const char *name, ip_addr_t *ipaddr, void *arg){
     struct espconn* conn = arg;
+    ip_addr_t broadcast;
     if(ipaddr == NULL){
-        os_printf("Logger: coun't get IP address for %s", name);
-        return;
+        os_printf("Logger: couldn't resolve IP address for %s; will broadcast instead\n", name);
+//        return;
+
+        
+        broadcast.addr = (uint32)0x0A00007B;
+        IP4_ADDR(&broadcast, 10,0,0,123);
+        os_printf("Falling back on %x for logging\n",broadcast.addr);
+        ipaddr = &broadcast;
     }
     
-    os_printf("Successfully resolved %s\n", name);
+    os_printf("Successfully resolved %s as %x\n", name, ipaddr->addr);
     
     if(master_addr.addr == 0 && ipaddr->addr != 0){
         master_addr.addr = ipaddr->addr;
         os_memcpy(conn->proto.udp->remote_ip, &ipaddr->addr, 4);
+        os_printf("Will send to %d.%d.%d.%d\n", (int)conn->proto.udp->remote_ip[0], (int)conn->proto.udp->remote_ip[1], (int)conn->proto.udp->remote_ip[2], (int)conn->proto.udp->remote_ip[3]);
         conn->proto.udp->local_port = espconn_port();
     }
     
@@ -68,9 +82,12 @@ static void dnsLookupCb(const char *name, ip_addr_t *ipaddr, void *arg){
 }
 
 void tempdInit(void){
+	os_printf("Temperature logging initialising\n");
 	tempConn.type=ESPCONN_UDP;
 	tempConn.state=ESPCONN_NONE;
 	tempConn.proto.udp=&socket;
+    
+	readDS18B20();
         
         master_addr.addr = 0;
         socket.remote_port=DATA_PORT;
